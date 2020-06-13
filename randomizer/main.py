b522bdb8d6b2f605d112302d1c67aaa9d66a537c
Tomaszal <mrtomaszal@gmail.com>
2020-06-13 21:42:31 +0100

    Added indicator window

--- randomizer/main.py
+++ randomizer/main.py
@@ -1,9 +1,12 @@
 import sys
-from PyQt5.QtWidgets import QDialog, QApplication
+
+# pylint: disable-msg=no-name-in-module
+from PyQt5 import QtGui, QtCore, QtWidgets
+from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow
 from dialog import Ui_Dialog
 
 
-class AppWindow(QDialog):
+class WeightsDialog(QDialog):
     def __init__(self):
         super().__init__()
         self.ui = Ui_Dialog()
@@ -11,7 +14,43 @@ class AppWindow(QDialog):
         self.show()
 
 
-app = QApplication(sys.argv)
-w = AppWindow()
-w.show()
-sys.exit(app.exec_())
+class IndicatorWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.setupWindow()
+        self.setupWidgets()
+
+    def setupWindow(self):
+        self.setWindowFlags(
+            QtCore.Qt.WindowStaysOnTopHint |
+            QtCore.Qt.FramelessWindowHint |
+            QtCore.Qt.X11BypassWindowManagerHint
+        )
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground, True)
+        self.setGeometry(
+            QtWidgets.QStyle.alignedRect(
+                QtCore.Qt.LeftToRight,
+                QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter,
+                QtCore.QSize(50, 50),
+                QtWidgets.qApp.desktop().availableGeometry()
+            ).adjusted(-20, 0, -20, 0)
+        )
+
+    def setupWidgets(self):
+        label = QtWidgets.QLabel()
+        label.setStyleSheet(
+            'background-color: rgba(0, 0, 0, 75%); border-radius: 10%;'
+        )
+        icon = QtGui.QIcon.fromTheme('shuffle')
+        label.setPixmap(icon.pixmap(50, 50, mode=QtGui.QIcon.Active))
+        self.setCentralWidget(label)
+
+    def mousePressEvent(self, event):
+        QtWidgets.qApp.quit()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = IndicatorWindow()
+    w.show()
+    sys.exit(app.exec_())
